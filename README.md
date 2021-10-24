8193c394f5e5029ccfe8d97b6372b12cf5590e97
Yoann <ybuzenet@gmail.com>
2021-10-24 23:08:35 +0200

    no idea what I'm doing

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # Let's see what we can do
 
-First step : CRA to test stuff
+First step : CRA to test stuff<br/>
 Second - Nextjs if want to do more stuff
---
+++ src/components/Map/Map.jsx
@@ -0,0 +1,11 @@
+import "./map.css";
+
+const Map = () => {
+  return (
+    <div className="map_container">
+      <div className="map">This is a map</div>
+    </div>
+  );
+};
+
+export default Map;
